1d5b2b9270dab33ec6c2aa1dd20868064d8dc016
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-08-07 16:21:04 +0530

    basic update

--- index.php
+++ index.php
@@ -12,7 +12,18 @@ $message = $data->message->text;
 if ($message == '/start') {
     $msg = "Welcome. Please enter your roll Number";
 } else {
-    $msg = "Other message";
+    $message = mysqli_real_escape_string($connect,$message);
+    $query = "SELECT * FROM students WHERE roll_number=$message";
+    $result = mysqli_query($connect,$query);
+    $row = mysqli_fetch_assoc($result);
+    if($row)
+    {   $name = $row['name'];
+        $msg = "Hi $name";
+    }
+    else
+    {
+        $msg = "Invalid Roll Number";
+    }
 }
 
 $URL = "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$CHAT_ID&text=$msg";
