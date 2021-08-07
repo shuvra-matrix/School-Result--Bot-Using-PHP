4ce8ed5585cadd11c631c7f6f5fe8a5415c9329a
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2021-08-07 16:11:19 +0530

    UPDATE CURL

--- index.php
+++ index.php
@@ -7,30 +7,18 @@ $data = json_decode($input);
 
 $TOKEN = "1938656199:AAGEcOUNta3yqFSC2geIr3vKCgsREY021TQ";
 
-$TEXT = "hello Shuvra";
 $CHAT_ID = $data->message->chat->id;
 $message = $data->message->text;
 if ($message == '/start') {
-    $msg = "Welcome. %OAPlease enter your roll Number";
+    $msg = "Welcome. Please enter your roll Number";
 } else {
     $msg = "Other message";
 }
 
 $URL = "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$CHAT_ID&text=$msg";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $URL);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-$result = json_decode($result, true);
-curl_close($ch);
-echo "<pre></pre>";
-print_r($result);
+file_get_contents($URL)
 
 ?>
 
 
 
-
-
-
-https://api.telegram.org/bot1938656199:AAGEcOUNta3yqFSC2geIr3vKCgsREY021TQ/setWebhook?url=http://shuvraproject.epizy.com/index.php
